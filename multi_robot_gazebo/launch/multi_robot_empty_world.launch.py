2625cb25615d498d76001fd1948da21b8f1ba628
Iesus Davila <iesusdavila@gmail.com>
2024-03-24 09:37:56 +0900

    Edicion de mundo abierto para mundos personales

--- multi_robot_gazebo/launch/multi_robot_empty_world.launch.py
+++ multi_robot_gazebo/launch/multi_robot_empty_world.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     navigation = LaunchConfiguration('navigation', default='false')
 
     world = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'),
+        get_package_share_directory('multi_robot_gazebo'),
         'worlds',
         'empty_world.world'
     )
