1a12d72def94add877588a319133d9d97d14eb4c
Iesus Davila <iesusdavila@gmail.com>
2024-04-08 10:32:58 +0900

    Funcionamiento basico del maestro

--- multi_robot_move/scripts/nav_through_poses.py
+++ multi_robot_move/scripts/nav_through_poses.py
@@ -10,7 +10,7 @@ from rclpy.duration import Duration
 import asyncio
 import sys
 
-async def navigate_robot(nav_client, goal_poses_robot, nav_start):
+async def navigate_robot_master(nav_client, goal_poses_robot, nav_start):
     pose_utils = PoseUtils()
 
     nav_client.followWaypoints(goal_poses_robot)
@@ -33,11 +33,42 @@ async def navigate_robot(nav_client, goal_poses_robot, nav_start):
                 nav_client.cancelTask()
 
             # Some follow waypoints request change to demo preemption
-            if now - nav_start > Duration(seconds=120.0):
-                goal_pose4 = pose_utils.create_pose(-1.5, 0.5, 0.0, 0.0)
-                goal_poses_robot = [goal_pose4]
-                nav_start = now
-                nav_client.followWaypoints(goal_poses_robot)
+            if now - nav_start > Duration(seconds=30.0):
+                print("Tiempo de navegación excedido")
+
+async def navigate_robot(nav_client, goal_poses_robot, nav_start, name_master_robot):
+    pose_utils = PoseUtils()
+
+    nav_client.followWaypoints(goal_poses_robot)
+    while not nav_client.isTaskComplete():
+        await asyncio.sleep(1)  # Espera corta para permitir que otras tareas se ejecuten
+        feedback = nav_client.getFeedback()
+        if feedback:
+            print(
+                'Executing current waypoint '
+                + str(nav_client.getNameRobot())
+                + ': '
+                + str(feedback.current_waypoint + 1)
+                + '/'
+                + str(len(goal_poses_robot))
+            )
+            now = nav_client.get_clock().now()
+
+            # Some navigation timeout to demo cancellation
+            if now - nav_start > Duration(seconds=600.0):
+                nav_client.cancelTask()
+
+            # Some follow waypoints request change to demo preemption
+            if now - nav_start > Duration(seconds=50.0):
+                print("Tiempo de navegación excedido")
+                nav_client.cancelTask()
+
+                nav_master = BasicNavigator(namespace=name_master_robot)
+                pose_goal = pose_utils.create_pose(1.5, -0.5, 0.0, 0.0)
+
+                nav_start = nav_master.get_clock().now()
+
+                await asyncio.gather(navigate_robot_master(nav_master, [pose_goal], nav_start)) 
 
 async def main(args=None):
     if args is None:
@@ -58,15 +89,20 @@ async def main(args=None):
     for robot in data_nav_robots.generate_robots():
 
         name_robot = robot['name']
-        navigation_client = BasicNavigator(namespace=name_robot)
 
-        list_poses_wo_process = data_nav_robots.get_list_poses(robot)  # obtener lista de poses sin convertir en PoseStamped
-        list_poses_w_process = pose_utils.create_poses(list_poses_wo_process)  # convertir a PoseStamped
-        goal_poses_robot = list_poses_w_process
+        if not robot['is_master']:
+            navigation_client = BasicNavigator(namespace=name_robot)
 
-        nav_start = navigation_client.get_clock().now()
+            list_poses_wo_process = data_nav_robots.get_list_poses(robot)  # obtener lista de poses sin convertir en PoseStamped
+            list_poses_w_process = pose_utils.create_poses(list_poses_wo_process)  # convertir a PoseStamped
+            goal_poses_robot = list_poses_w_process
+            name_master_robot = robot['name_master']
 
-        list_funciones.append(navigate_robot(navigation_client, goal_poses_robot, nav_start))
+            nav_start = navigation_client.get_clock().now()
+
+            list_funciones.append(navigate_robot(navigation_client, goal_poses_robot, nav_start, name_master_robot))
+        else:
+            print("Master robot: " + name_robot)
 
     await asyncio.gather(*list_funciones)
 
