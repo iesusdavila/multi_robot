f263718c23256e6a8690fa800abe6b48b21fad79
Iesus Davila <iesusdavila@gmail.com>
2024-06-11 09:34:54 -0500

    Mejora en el patron de diseño

--- multi_robot_master_slave/scripts/utils/delegate_task.py
+++ multi_robot_master_slave/scripts/utils/delegate_task.py
@@ -1,69 +1,88 @@
 from abc import ABC, abstractmethod
+from typing import Any, Optional
 
 class Handler(ABC):
-    def __init__(self, successor=None):
-        self._successor = successor
-
     @abstractmethod
-    def handle_request(self, request):
+    def set_next(self, handler: 'Handler') -> 'Handler':
         pass
 
-class FreeSlaveHandler(Handler):
-    def handle_request(self, request):
+    @abstractmethod
+    def handle(self, request: dict) -> Optional[Any]:
+        pass
+
+class AbstractHandler(Handler):
+    _next_handler: Handler = None
+
+    def set_next(self, handler: Handler) -> Handler:
+        self._next_handler = handler
+        return handler
+
+    @abstractmethod
+    def handle(self, request: dict) -> Optional[Any]:
+        if self._next_handler:
+            return self._next_handler.handle(request)
+        return None
+
+class FreeSlaveHandler(AbstractHandler):
+    def handle(self, request: dict) -> Optional[Any]:
+        nav_slave = request['system_master_slave'][request['name_master']]["slaves"][request['name_slave']]["nav_class"]
+        nav_slave.info("---> Buscando esclavo libre que pueda realizar la tarea... <---")
         found_free_slave, free_slave = self.find_free_slave(request['name_master'], request['system_master_slave'])
         if found_free_slave:
+            request['slaves'][free_slave]["nav_class"].info(f'Para el esclavo {request["name_slave"]}, yo el robot {free_slave} estoy libre.')
             nav_free_slave = request['slaves'][free_slave]["nav_class"]
-            request['slaves'][free_slave]["task_queue"][request['name_slave']] = request['goal_poses'][request['current_waypoint']:]
             return nav_free_slave, True
-        elif self._successor:
-            return self._successor.handle_request(request)
-        return None, False
+        else:
+            return super().handle(request)
 
     def find_free_slave(self, name_master, system_master_slave):
-        print("---> Buscando esclavo libre que pueda realizar la tarea... <---")
         find_free_slave = False
         slave = None
         
         list_slaves = system_master_slave[name_master]["slaves"]
         for name_slave_iter in list_slaves:
-            if len(list_slaves[name_slave_iter]["task_queue"]) == 0:
+            if len(list_slaves[name_slave_iter]["task_queue"]) == 0 and list_slaves[name_slave_iter]["status"] == True:
                 find_free_slave = True
                 slave = name_slave_iter
                 break
         
         return find_free_slave, slave
 
-class SlaveWithOneTaskHandler(Handler):
-    def handle_request(self, request):
+class SlaveWithOneTaskHandler(AbstractHandler):
+    def handle(self, request: dict) -> Optional[Any]:
+        nav_slave = request['system_master_slave'][request['name_master']]["slaves"][request['name_slave']]["nav_class"]
+        nav_slave.info("---> Buscando esclavo con una tarea pendiente que pueda realizar la tarea... <---")
         found_slave_with_one_task, slave_with_one_task = self.find_slave_with_one_task(request['name_master'], request['name_slave'], request['system_master_slave'])
         if found_slave_with_one_task:
+            request['slaves'][slave_with_one_task]["nav_class"].info(f'Para el esclavo {request["name_slave"]}, yo el robot {slave_with_one_task} tengo una tarea pendiente.')
             nav_slave_with_one_task = request['slaves'][slave_with_one_task]["nav_class"]
-            request['slaves'][slave_with_one_task]["task_queue"][request['name_slave']] = request['goal_poses'][request['current_waypoint']:]
             return nav_slave_with_one_task, True
-        elif self._successor:
-            return self._successor.handle_request(request)
-        return None, False
+        else:
+            return super().handle(request)
 
     def find_slave_with_one_task(self, name_master, name_slave, system_master_slave):
-        print("---> Buscando esclavo con una tarea pendiente que pueda realizar la tarea... <---")
         find_slave_with_one_task = False
         slave = None
         
         list_slaves = system_master_slave[name_master]["slaves"]
         for name_slave_iter in list_slaves:
-            if len(list_slaves[name_slave_iter]["task_queue"]) == 1:
+            if len(list_slaves[name_slave_iter]["task_queue"]) == 1 and list_slaves[name_slave_iter]["status"] == True and name_slave_iter != name_slave:
                 find_slave_with_one_task = True
                 slave = name_slave_iter
                 break
         
         return find_slave_with_one_task, slave
 
-class MasterHandler(Handler):
-    def handle_request(self, request):
+class MasterHandler(AbstractHandler):
+    def handle(self, request: dict) -> Optional[Any]:
         nav_master = request['system_master_slave'][request['name_master']]["nav_class"]
         request['system_master_slave'][request['name_master']]["slave_tasks"][request['name_slave']] = request['goal_poses'][request['current_waypoint']:]
-        self.is_master_busy(nav_master.getFeedback())
-        return nav_master, False
+        if request['system_master_slave'][request['name_master']]["slave_tasks"][request['name_slave']]:
+            self.is_master_busy(nav_master.getFeedback())
+            nav_master.info(f'Para el esclavo {request["name_slave"]}, yo el robot {request["name_master"]} soy el maestro para ejecutar su tarea.')
+            return nav_master, False
+        else:
+            return super().handle(request)
 
     def is_master_busy(self, nav_master_feedback):
         if nav_master_feedback is None:
