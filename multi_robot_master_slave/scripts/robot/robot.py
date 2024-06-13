1cd1364fb179ad27901215dc88f9aa8836c30477
Iesus Davila <iesusdavila@gmail.com>
2024-06-12 22:45:57 -0500

    Ingreso de mensaje de tiempo maximo opc

--- multi_robot_master_slave/scripts/robot/robot.py
+++ multi_robot_master_slave/scripts/robot/robot.py
@@ -2,12 +2,14 @@ class Robot():
     def __init__(self):
         pass
     
-    def generate_message(self, name_robot, current_waypoint, number_poses, nav_time=(0,0,0), max_time=(0,0,0), name_slave=None):
+    def generate_message(self, name_robot, current_waypoint, number_poses, nav_time=(0,0,0), max_time=None, name_slave=None):
         hour_nav, min_nav, sec_nav = nav_time
-        hour_max, min_max, sec_max = max_time
-
         msg = 'Executing current waypoint ' + str(name_robot) + ': '+ str(current_waypoint + 1) + '/' + str(number_poses) 
-        msg += ' - ' + str(hour_nav-19) + ':' + str(min_nav) + ':' + str(sec_nav) + ' / ' + str(hour_max-19) + ':' + str(min_max) + ':' + str(sec_max)
+        msg += ' - ' + str(hour_nav-19) + ':' + str(min_nav) + ':' + str(sec_nav) 
+
+        if max_time is not None:
+            hour_max, min_max, sec_max = max_time
+            msg += ' / ' + str(hour_max-19) + ':' + str(min_max) + ':' + str(sec_max)
 
         if name_slave is not None: 
             msg += " del esclavo: " + name_slave
